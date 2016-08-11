bcf65f2273cc498e25665651f6720f986b8a5acc
Rommel M. Zamora <rmzamora@Mell-MacBook-Pro.local>
2016-08-11 09:59:07 +0800

    Add BrueryOAuthBundle to Bassoon. Secure /api link.

---
+++ src/AppBundle/Entity/OAuthServer/Client.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the Bruery Platform.
+ *
+ * (c) Viktore Zara <viktore.zara@gmail.com>
+ * (c) Mell Zamorw <mellzamora@outlook.com>
+ *
+ * Copyright (c) 2016. For the full copyright and license information, please view the LICENSE  file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity\OAuthServer;
+
+use Bruery\OAuthServerBundle\Entity\BaseClient;
+
+class Client extends BaseClient
+{
+    /**
+     * @var integer $id
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
